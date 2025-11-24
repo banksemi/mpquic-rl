53247a67291ab8e5f34ece07fe2e9d603c054747
Seunghwa Lee <banksemi@naver.com>
2025-11-24 13:16:33 +0900

    chore: Integrate cert script for easier management

--- MAppLE/dash/makecert.sh
+++ MAppLE/dash/makecert.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-openssl req -x509 -newkey rsa:2048 -keyout privkey.pem -out cert.pem -days 356 -nodes
+#!/bin/bash
+mkdir -p /certs
+cd /certs
+SUBJECT="/CN=10.0.0.20"
+openssl rand -writerand /root/.rnd
+if [ -f "privkey.pem" ]; then
+    # If key exists, renew certificate only (using existing key)
+    openssl req -x509 -key privkey.pem -out cert.pem -days 365 -nodes -subj "$SUBJECT"
+else
+    # If key doesn't exist, create new key and certificate
+    openssl req -x509 -newkey rsa:2048 -keyout privkey.pem -out cert.pem -days 365 -nodes -subj "$SUBJECT"
+fi
