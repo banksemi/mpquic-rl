fd1569c27daead4e29c3f3058502ffdea85bf569
Seunghwa Lee <banksemi@naver.com>
2025-11-24 13:01:23 +0900

    fix: Correct video chunk preparation for MPQUIC simulation

--- MAppLE/dash/prepare_video.sh
+++ MAppLE/dash/prepare_video.sh
@@ -1,14 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-SRC="jellyfish.mkv"
-SRCURL="http://www.jell.yfish.us/media/jellyfish-100-mbps-hd-h264.mkv"
-
-if [ ! -f "$SRC" ]; then
-    wget $SRCURL -O $SRC
-fi
+set -xe
 
 function encode {
-    mkdir -p video$1s
+    mkdir -p /videos/video$1s
 
     ffmpeg -re -stream_loop 4 -i $SRC -c:v libx264 \
         -map 0 -b:v:0 10M -maxrate:v:0 10M -s:v:0 2560x1440 -profile:v:0 baseline \
@@ -18,24 +13,7 @@ function encode {
         -b_strategy 0 -use_template 0 -use_timeline 1 \
         -seg_duration $1 -streaming 1 \
         -adaptation_sets "id=0,streams=v" \
-        -f dash video$1s/manifest.mpd
-
-    ls -l video/*stream0* | awk '{print $5}' > chunksizes-stream0.txt
-    ls -l video/*stream1* | awk '{print $5}' > chunksizes-stream1.txt
-    ls -l video/*stream2* | awk '{print $5}' > chunksizes-stream2.txt
+        -f dash /videos/video$1s/manifest.mpd
 }
 
-encode 0.1
-encode 0.2
-encode 0.3
-encode 0.4
-encode 0.5
-encode 0.6
-encode 0.7
-encode 0.8
-encode 0.9
-encode 1
-encode 2
-encode 3
-encode 4
-encode 5
+encode $1
